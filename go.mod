28b8edf92f6da73987f1fedfc12cdcfc7369f0cc
管竹 <guanzhu@ainnovation.com>
2020-04-16 14:49:04 +0800

    add casbin

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.14
 
 require (
 	github.com/appleboy/gin-jwt/v2 v2.6.3
+	github.com/casbin/casbin/v2 v2.2.1
+	github.com/casbin/gorm-adapter/v2 v2.0.3
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/kr/pretty v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"time"
 
 	"github.com/appleboy/gin-jwt/v2"
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v2"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type login struct {
@@ -15,11 +18,60 @@ type login struct {
 	Password string `form:"password" json:"password" binding:"required"`
 }
 
-// User demo
-type User struct {
-	UserName  string
-	FirstName string
-	LastName  string
+const IdentityKey = "username"
+
+func Authorization(e *casbin.Enforcer) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		username, exists := c.Get("username")
+		if exists == false {
+			fmt.Println("username not exists")
+			c.Abort()
+			return
+		}
+
+		fmt.Println("Authorization username: ", username)
+
+		if username == "" {
+			fmt.Println("headers invalid")
+			c.JSON(200, gin.H{
+				"code":    401,
+				"message": "Unauthorized",
+				"data":    "",
+			})
+			c.Abort()
+			return
+		}
+
+		// 请求的path
+		p := c.Request.URL.Path
+		// 请求的方法
+		m := c.Request.Method
+		// 这里认证
+		res, err := e.Enforce(username, p, m)
+
+		if err != nil {
+			fmt.Println("no permission")
+			fmt.Println(err)
+			c.JSON(200, gin.H{
+				"code":    401,
+				"message": "Unauthorized",
+				"data":    "",
+			})
+			c.Abort()
+			return
+		}
+		if !res {
+			fmt.Println("permission check failed")
+			c.JSON(200, gin.H{
+				"code":    401,
+				"message": "Unauthorized",
+				"data":    "",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
 
 func main() {
@@ -42,45 +94,31 @@ func main() {
 			username := loginVal.Username
 			password := loginVal.Password
 
-			if (username == "admin" && password == "admin") || (username == "test" && password == "test") {
-				return &User{
-					UserName:  username,
-					LastName:  "Bo-Yi",
-					FirstName: "Wu",
-				}, nil
+			if (username == "administrator" && password == "administrator") || (username == "test" && password == "test") {
+				return username, nil
 			}
 
 			return nil, jwt.ErrFailedAuthentication
 		},
 		// 授权
-		Authorizator: func(data interface{}, c *gin.Context) bool {
-			if v, ok := data.(*User); ok && v.UserName == "admin" {
-				return true
-			}
-
-			return false
-		},
+		Authorizator: nil,
+		// 添加payload到jwt
 		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			if v, ok := data.(*User); ok {
+			if _, ok := data.(string); ok {
 				return jwt.MapClaims{
-					"id": v.UserName,
+					IdentityKey: data,
 				}
 			}
 			return jwt.MapClaims{}
 		},
-		Unauthorized: func(c *gin.Context, code int, message string) {
-			c.JSON(http.StatusOK, gin.H{
-				"code":    code,
-				"message": "message",
-				"status":  "Unauthorized",
-			})
-		},
+		// 未授权响应
+		Unauthorized:    nil,
 		LoginResponse:   nil,
 		LogoutResponse:  nil,
 		RefreshResponse: nil,
-		//IdentityHandler:       nil,
-		//IdentityKey:           "",
-		TokenLookup: "header: Authorization",
+		IdentityHandler: nil,
+		IdentityKey:     IdentityKey,
+		TokenLookup:     "header: Authorization",
 		//TokenHeadName:         "",
 		//TimeFunc:              nil,
 		//HTTPStatusMessageFunc: nil,
@@ -99,12 +137,24 @@ func main() {
 		log.Fatal("JWT Error:" + err.Error())
 	}
 
+	a, _ := gormadapter.NewAdapter("mysql", "root:root@tcp(localhost)/")
+	e, _ := casbin.NewEnforcer("authz_model.conf", a)
+	e.EnableLog(true)
+
+	_, _ = e.AddPolicy("role:admin", "*", "*")
+	_, _ = e.AddRoleForUser("administrator", "role:admin")
+
+	//从DB加载策略
+	_ = e.LoadPolicy()
+
 	r.POST("/login", authMiddleware.LoginHandler)
 
 	auth := r.Group("/v1")
-	auth.Use(authMiddleware.MiddlewareFunc())
+	auth.Use(authMiddleware.MiddlewareFunc(), Authorization(e))
 	{
 		auth.GET("/test", func(c *gin.Context) {
+			username, _ := c.Get(IdentityKey)
+			fmt.Printf("IdentityKey username: %v\n", username)
 			c.JSON(http.StatusOK, gin.H{
 				"hello": "world",
 			})
